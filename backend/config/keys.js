f73ce4d8a9671a6ec259fd17de8e3f6c7be4fc16
Nishant Bhickta <nishant.b@solarsquare.in>
2024-08-20 15:56:53 +0530

    fix: properly added .env file to backend

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -9,7 +9,7 @@ module.exports = {
   prelimsSheetID: "1aPa_Ru4vCF5uwV22RlbjwKhbII44xowcBO7LsxJ-rP0",
   visionSheetID: "10nZGdtDP8PE6U9Ue2JdudnGaJnuuyEmUpT0bcvYqNhw",
   subscriptionSheetID: "1tIkH0cPwfw5tkknWmxolAoW15cHsjnR30u4cq6gMOdA",
-  sheetApi: process.env.GOOGLE_SHEET_API, // only viewer access via sheetApi
+  sheetApi: process.env.GOOGLE_SHEET_API,
   slackApiUrl: process.env.SLACK_API_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
